f2a07c603192b9fea8a533250e204cf497afe03c
Rudra Gupta <114164855+Rudragupta-1@users.noreply.github.com>
2024-03-25 23:52:15 +0530

    Create heightOfBinaryTree.java

---
+++ Trees/heightOfBinaryTree.java
@@ -0,0 +1,25 @@
+//User function Template for Java
+
+/* 
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+
+class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        if(node==null)
+        return 0;
+        return Math.max(height(node.left),height(node.right))+1;
+    }
+}
